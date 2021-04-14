934837f0e3cce1d65607387e709970e737f29a27
Alejandro Muñoz Gutierrez <alejandro.mg.0505@gmail.com>
2021-04-14 19:27:28 +0200

    Finished

---
+++ idiomas_boton/boton.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace idiomas_boton
+{
+    partial class boton
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 65);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // boton
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button1);
+            this.Name = "boton";
+            this.Size = new System.Drawing.Size(114, 72);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ idiomas_boton/boton.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace idiomas_boton
+{
+    public partial class boton : UserControl
+    {
+
+        public int state;
+        public boton()
+        {
+            InitializeComponent();
+            button1.BackgroundImage = Image.FromFile("C:/Users/lolac/source/repos/GourmetteRestaurante/assets/bandera_spain.png");
+            state = 1;
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (state == 1)
+            {
+                button1.BackgroundImage = Image.FromFile("C:/Users/lolac/source/repos/GourmetteRestaurante/assets/bandera_ing.png");
+                state = 2;
+            }
+            else if (state == 2)
+            {
+                button1.BackgroundImage = Image.FromFile("C:/Users/lolac/source/repos/GourmetteRestaurante/assets/bandera_spain.png");
+                state = 1;
+            }
+
+        }
+    }
+}
